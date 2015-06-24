7f22a5cc57fd285ed2b12714c7b32ca1f7786c14
Stuart <runfaj@gmail.com>
2015-06-24 00:32:28 -0600

    :+1:

--- app/src/main/java/com/stuartrosk/littleworlds/app/ImageEditFragment.java
+++ app/src/main/java/com/stuartrosk/littleworlds/app/ImageEditFragment.java
@@ -2,16 +2,15 @@ package com.stuartrosk.littleworlds.app;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.content.Intent;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class ImageEditFragment extends Fragment {
 
@@ -19,7 +18,9 @@ public class ImageEditFragment extends Fragment {
     private View view;
     private ImageEditFragmentListener listener;
     private ViewGroup radioGroup;
-    private Button saveBtn, cancelBtn;
+    private Button saveBtn, cancelBtn, sizeBtn, imageBtn;
+    private ImageJsonObject config;
+    private TextView titleText;
 
     public ImageEditFragment(){}
 
@@ -65,7 +66,8 @@ public class ImageEditFragment extends Fragment {
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_image_edit, container, false);
 
-        preferences = getActivity().getPreferences(getActivity().MODE_PRIVATE);
+        preferences = getActivity().getSharedPreferences("com.stuartrosk.littleworlds", Context.MODE_PRIVATE);
+        String editPos = getArguments().getString("editPos","");
 
         //add various handlers
         radioGroup = (ViewGroup) view.findViewById(R.id.edit_radiogroup);
@@ -81,6 +83,11 @@ public class ImageEditFragment extends Fragment {
         }
         cancelBtn = (Button)view.findViewById(R.id.cancelBtn);
         saveBtn = (Button)view.findViewById(R.id.saveBtn);
+        sizeBtn = (Button)view.findViewById(R.id.sizeBtn);
+        imageBtn = (Button)view.findViewById(R.id.imageBtn);
+        titleText = (TextView)view.findViewById(R.id.imageEditTitleTxt);
+
+        config = new ImageJsonObject(getActivity(), ImageJsonObject.Position.valueOf(editPos));
 
         cancelBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -98,8 +105,11 @@ public class ImageEditFragment extends Fragment {
             }
         });
 
-        String editPos = getArguments().getString("editPos","");
-        Log.d("test",editPos);
+        titleText.setText("Editing " + config.getPositionName());
+        sizeBtn.setText(config.width + "x" + config.height);
+        String imageText = config.file_name;
+        if(imageText.equals("")) imageText = getString(R.string.default_image_btn_text);
+        imageBtn.setText(imageText);
 
         return view;
     }
